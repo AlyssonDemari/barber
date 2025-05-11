1f63ff65271b1c6ee18a6a62d902ca0b72fd03ad
AlyssonDemari <alyssondemari@gmail.com>
2025-05-11 20:16:28 -0300

    adição do slider

--- JS/index.js
+++ JS/index.js
@@ -4,3 +4,37 @@ menu.addEventListener('click', () => {
     const menuMobile = document.querySelector('.menu-mobile');
     menuMobile.classList.toggle('active');
 });
+
+const images = [
+    { 'id': '1', 'url': 'corte1.jpg' },
+    { 'id': '2', 'url': 'corte2.jpg' },
+    { 'id': '3', 'url': 'corte3.jpg' },
+    { 'id': '4', 'url': 'corte1.jpg' },
+    { 'id': '5', 'url': 'corte2.jpg' },
+];
+
+const containerItens = document.querySelector('#container-item');
+let currentIndex = 0;
+
+const loadImages = (images, container) => {
+    images.forEach(image => {
+        container.innerHTML += `
+            <div class='item'>
+                <img src='JS/${image.url}'/>
+            </div>
+        `;
+    });
+};
+
+loadImages(images, containerItens);
+
+const nextSlide = () => {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateCarousel();
+};
+
+const updateCarousel = () => {
+    containerItens.style.transform = `translateX(-${currentIndex * 100}%)`;
+};
+
+setInterval(nextSlide, 2000);
